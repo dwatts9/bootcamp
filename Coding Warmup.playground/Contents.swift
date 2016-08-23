feead2ffa21e299cb222dbfc7d241e90d856db6b
dwatts9 <Damienwatts106@gmail.com>
2016-08-22 22:46:43 -0500

    adding all files

---
+++ Coding Warmup.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var name = "Hello Damien!"
+
+var someString = "String"
+
+var aFloat: Float = 2.1
+
+var aDec: Decimal = 3.212
+
+//var anInt: Int = 10
+//
+//var bankAccountBalance = -200.54
+
+var age = 25 //Type Inference - this is initialized with data
+var age2: Int //Explicit - this one is not initialized
+age2 = 35
+print(age2)
+
+var fName = "Molly"
+var lName = "Minton"
+var fullName = fName + " " + lName
+
+var length = 50
+var width = 10
+
+var area = length * width
+
+var phrase = "Hi, my name is \(fName) \(lName)"//String interpolation
+
+var div = 50 / 9
+var remainder = 50 % 9
+
+
---
+++ README.md.txt
@@ -0,0 +1,4 @@
+#######readme
+
+1)adding this shit to github
+2)love this man
